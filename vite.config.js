a06abb95fa83f19a779c112ee5fdfba7e6b483d6
didarkhan97 <didark791@gmail.com>
2024-05-01 19:20:14 -0400

    website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/' // Replace `[repo-name]` with your actual repository name
+  plugins: [react()]
 });
